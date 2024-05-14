9cc18741b16d79c9a28a6f5dc46d1387ef9d6827
HikiJun97 <sgn04088@gmail.com>
2024-05-14 17:05:48 +0900

    fix(TimeElement.mjs): use cdn

--- src/TimeElement.mjs
+++ src/TimeElement.mjs
@@ -1,4 +1,8 @@
-import { LitElement, css, html } from "lit";
+import {
+  LitElement,
+  html,
+  css,
+} from "https://cdn.jsdelivr.net/gh/lit/dist@3/core/lit-core.min.js";
 
 export class TimeElement extends LitElement {
   static styles = css`
